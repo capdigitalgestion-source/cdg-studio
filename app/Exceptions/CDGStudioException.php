59680b893867ffd2d321b33d2c615ea3b25265dc
David Forrez <contact@capdigitale.fr>
2026-06-30 15:57:13 +0200

    Ajout des exceptions du framework

---
+++ app/Exceptions/CDGStudioException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CDGStudio\Exceptions;
+
+use Exception;
+
+class CDGStudioException extends Exception
+{
+}
---
+++ app/Exceptions/ConfigurationException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CDGStudio\Exceptions;
+
+class ConfigurationException extends CDGStudioException
+{
+}
