f3fab2def72246e0771e53fe0bba01401936bd2c
David Forrez <contact@capdigitale.fr>
2026-07-01 19:35:23 +0200

    Ajout de la persistance du module KeyFigures

--- app/Modules/KeyFigures/KeyFiguresModule.php
+++ app/Modules/KeyFigures/KeyFiguresModule.php
@@ -4,22 +4,12 @@ declare(strict_types=1);
 
 namespace CDGStudio\Modules\KeyFigures;
 
-use CDGStudio\Core\AbstractModule;
-use CDGStudio\Modules\KeyFigures\Repositories\KeyFiguresRepository;
+use CDGStudio\Contracts\ModuleInterface;
+use CDGStudio\Modules\KeyFigures\KeyFiguresRepository;
 
-final class KeyFiguresModule extends AbstractModule
+final class KeyFiguresModule implements ModuleInterface
 {
-    public function getName(): string
-    {
-        return 'key_figures';
-    }
-
-    public function getLabel(): string
-    {
-        return 'Key Figures';
-    }
-
-    public function boot(): void
+    public function register(): void
     {
         $repository = new KeyFiguresRepository();
 
