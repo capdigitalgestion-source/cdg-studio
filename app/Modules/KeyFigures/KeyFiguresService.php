bcc82d1fa5215a49d0549bb75f1a3e8996b9e346
David Forrez <contact@capdigitale.fr>
2026-07-01 18:47:36 +0200

    Ajoute les méthodes métier du service KeyFigures

--- app/Modules/KeyFigures/KeyFiguresService.php
+++ app/Modules/KeyFigures/KeyFiguresService.php
@@ -19,6 +19,26 @@ final class KeyFiguresService
         return $this->repository->findAll();
     }
 
+    public function find(int $id): ?array
+    {
+        return $this->repository->findById($id);
+    }
+
+    public function create(array $data): int
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function delete(int $id): bool
+    {
+        return $this->repository->delete($id);
+    }
+
     public function countAll(): int
     {
         return $this->repository->countAll();
