9811e7cb29d6d4ed6b31c808a84b8697508c837b
David Forrez <contact@capdigitale.fr>
2026-07-01 18:41:11 +0200

    Adapte la vue KeyFigures au modèle métier

--- app/Modules/KeyFigures/Views/index.php
+++ app/Modules/KeyFigures/Views/index.php
@@ -7,8 +7,6 @@ if (! defined('ABSPATH')) {
 }
 
 /**
- * Variables disponibles :
- *
  * @var array<int, array<string, mixed>> $figures
  * @var int $total
  * @var int $visible
@@ -36,17 +34,32 @@ if (! defined('ABSPATH')) {
     <table class="widefat striped">
         <thead>
             <tr>
+                <th>Ordre</th>
+                <th>Icône</th>
                 <th>Libellé</th>
                 <th>Valeur</th>
+                <th>Couleur</th>
+                <th>Animation</th>
                 <th>Visible</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($figures as $figure) : ?>
                 <tr>
+                    <td><?php echo esc_html((string) ($figure['display_order'] ?? '')); ?></td>
+                    <td>
+                        <span class="dashicons <?php echo esc_attr((string) ($figure['icon'] ?? '')); ?>"></span>
+                    </td>
                     <td><?php echo esc_html((string) ($figure['label'] ?? '')); ?></td>
-                    <td><?php echo esc_html((string) ($figure['value'] ?? '')); ?></td>
-                    <td><?php echo ! empty($figure['visible']) ? 'Oui' : 'Non'; ?></td>
+                    <td>
+                        <?php echo esc_html((string) ($figure['value'] ?? '')); ?>
+                        <?php echo esc_html((string) ($figure['suffix'] ?? '')); ?>
+                    </td>
+                    <td>
+                        <code><?php echo esc_html((string) ($figure['color'] ?? '')); ?></code>
+                    </td>
+                    <td><?php echo esc_html((string) ($figure['animation'] ?? '')); ?></td>
+                    <td><?php echo ! empty($figure['is_visible']) ? 'Oui' : 'Non'; ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
