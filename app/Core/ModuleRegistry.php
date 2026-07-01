ea14aea2db4fd9f201688a89f552f45dcca4d637
David Forrez <contact@capdigitale.fr>
2026-07-01 18:06:53 +0200

    Restauration du module Settings et initialisation du module KeyFigures

--- app/Core/ModuleRegistry.php
+++ app/Core/ModuleRegistry.php
@@ -7,15 +7,17 @@ namespace CDGStudio\Core;
 use CDGStudio\Modules\Dashboard\DashboardModule;
 use CDGStudio\Modules\KeyFigures\KeyFiguresModule;
 use CDGStudio\Modules\Settings\SettingsModule;
+
 final class ModuleRegistry
 {
-    /**Z
+    /**
      * @return array<class-string>
      */
     public static function all(): array
     {
         return [
             DashboardModule::class,
+            SettingsModule::class,
             KeyFiguresModule::class,
         ];
     }
